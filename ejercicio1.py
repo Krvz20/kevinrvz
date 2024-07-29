6218c214e1648c60c8aa7b56095cf5f2742099fa
SENA <kevinrodriguez000@hotmail.com>
2024-07-29 17:32:56 -0500

    subiendo archivos al repositorio

---
+++ ejercicio1.py
@@ -0,0 +1,22 @@
+mensaje="Hola, estoy aprendiendo Python" 
+nombre="Kevin"
+formacion="ADSO"
+edad=23
+estatura=1.90
+peso=80
+print(mensaje, "Mi nombre es:",nombre, "soy de la formacion:", formacion, "tengo:", edad, "años:", "mido:", "%.2f" % estatura, "y peso:", peso, "kg")
+print("\n")
+a=20
+b= 10
+suma= a + b 
+resta= a - b 
+multiplicar= a * b 
+dividir= a // b 
+print("El resultado de la suma es: ", suma, "\nEl resultado de la resta es: ", resta, "\nEl resultado de la multiplicar: ", multiplicar, "\nEL resultado de la divicion es: ", dividir )
+""" 
+Ejercicio pidiendo datos por teclado
+"""
+c=int(input("Digitar el primer numero "))
+g=int(input("Digitar el segundo numero "))
+print("El resultado de la suma es: ", c+g,"\nEl resultado de la resta es : ", c-g, "\nEl resultado de la multiplicacion es: ", c*g, "\nEl resultado de la divicion es: ", c//g )
+
